d934fd552c4782c95bc24455ea5457d83fbc1f9e
shzy2012 <shzy2012@gmail.com>
2019-10-28 15:38:16 +0800

    update tool

--- tool/tool.go
+++ tool/tool.go
@@ -6,36 +6,30 @@ import (
 	"time"
 )
 
-//Tool 工具集
-type Tool struct {
-	rand    *rand.Rand
-	counter uint64
-}
+var r *rand.Rand
+var counter uint64
 
-//T 创建 Tool
-func T() *Tool {
-	return &Tool{
-		rand: rand.New(rand.NewSource(time.Now().UnixNano())),
-	}
+func init() {
+	r = rand.New(rand.NewSource(time.Now().UnixNano()))
 }
 
 //GetRandomString 获取随机字符串
-func (t *Tool) GetRandomString(length uint64) string {
+func GetRandomString(length uint64) string {
 	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	bytes := []byte(str)
 	result := []byte{}
 	for i := uint64(0); i < length; i++ {
-		result = append(result, bytes[t.rand.Intn(int(len(bytes)))])
+		result = append(result, bytes[r.Intn(int(len(bytes)))])
 	}
 	return string(result)
 }
 
 //SetCounter 设置自增起始点
-func (t *Tool) SetCounter(base uint64) {
-	t.counter = base
+func SetCounter(base uint64) {
+	counter = base
 }
 
 //AutoIncrementing 自动自增
-func (t *Tool) AutoIncrementing() uint64 {
-	return atomic.AddUint64(&t.counter, 1)
+func AutoIncrementing() uint64 {
+	return atomic.AddUint64(&counter, 1)
 }
--- tool/tool_test.go
+++ tool/tool_test.go
@@ -6,27 +6,26 @@ import (
 )
 
 func Benchmark_GetRandomString(b *testing.B) {
-	tool := T()
+
 	for i := 0; i < b.N; i++ {
-		tool.GetRandomString(64)
+		GetRandomString(64)
 	}
 }
 
 func Test_AutoIncrementing(t *testing.T) {
-	tool := T()
+
 	//tool.SetCounter(999999999)
 	for n := 0; n <= 10; n++ {
 		go func(n int) {
 			for i := 0; i < 1000000000; i++ {
-				fmt.Println(n, "-", tool.AutoIncrementing())
+				fmt.Println(n, "-", AutoIncrementing())
 			}
 		}(n)
 	}
 }
 
 func Benchmark_AutoIncrementing(b *testing.B) {
-	tool := T()
 	for i := 0; i < b.N; i++ {
-		fmt.Println(tool.AutoIncrementing())
+		fmt.Println(AutoIncrementing())
 	}
 }
