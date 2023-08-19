e435ca99ed384e2e432c643644981a00f55ce8bf
zhouyi <shzy2012@gmail.com>
2023-08-19 11:36:20 +0800

    添加distinct slice

---
+++ tool/slice.go
@@ -0,0 +1,25 @@
+package tool
+
+func DistinectIntSlice(intSlice []int) []int {
+	keys := make(map[int]bool)
+	list := []int{}
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+func DistinectStringSlice(intSlice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
